9d9a059cb06d00a727636ec9855704ba75005847
Levi Durfee <levi.durfee@gmail.com>
2019-02-10 18:26:20 -0500

    Initial Commit

---
+++ main.go
@@ -0,0 +1,18 @@
+// Copyright 2018 Google Inc. All rights reserved.
+// Use of this source code is governed by the Apache 2.0
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"net/http"
+
+	"github.com/levidurfee/ham/handlers"
+
+	"google.golang.org/appengine"
+)
+
+func main() {
+	http.HandleFunc("/", handlers.Handler)
+	appengine.Main()
+}
