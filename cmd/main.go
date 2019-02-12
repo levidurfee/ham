0660b959fd3c60758a71f40d02a777f5ec6527ed
Levi Durfee <levi.durfee@gmail.com>
2019-02-12 10:04:21 -0500

    Add app engine user

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,35 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
 	"google.golang.org/appengine"
+	"google.golang.org/appengine/user"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hi"))
 }
 
+func welcome(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "text/html; charset=utf-8")
+	ctx := appengine.NewContext(r)
+	u := user.Current(ctx)
+	if u == nil {
+		url, _ := user.LoginURL(ctx, "/")
+		fmt.Fprintf(w, `<a href="%s">Sign in or register</a>`, url)
+		return
+	}
+	url, _ := user.LogoutURL(ctx, "/")
+	fmt.Fprintf(w, `Welcome, %s! (<a href="%s">sign out</a>)`, u, url)
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", handler)
+	r.HandleFunc("/user", welcome)
 
 	http.Handle("/", r)
 
