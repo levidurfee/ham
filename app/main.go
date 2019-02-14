e69a02fd0248eb999a1ac9750701ac195a0e6415
Levi Durfee <levi.durfee@gmail.com>
2019-02-14 14:46:30 -0500

    Redo homeHandler

--- app/main.go
+++ app/main.go
@@ -6,7 +6,15 @@ import (
 
 	"github.com/gorilla/mux"
 	"google.golang.org/appengine"
+	"google.golang.org/appengine/user"
 )
+
+// GOhamData is a struct for storing basic data
+type GOhamData struct {
+	LoggedIn bool
+	Login    string
+	Logout   string
+}
 
 func main() {
 	r := mux.NewRouter()
@@ -17,6 +25,22 @@ func main() {
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
+	u := user.Current(ctx)
+	var g GOhamData
+	if u == nil {
+		g.LoggedIn = false
+	}
+	login, _ := user.LoginURL(ctx, "/login")
+	logout, _ := user.LogoutURL(ctx, "/")
+
+	g.Login = login
+	g.Logout = logout
+
+	renderTemplate(w, g)
+}
+
+func renderTemplate(w http.ResponseWriter, d GOhamData) {
 	tmpl := template.Must(template.ParseFiles("../templates/base.html", "../templates/home.html"))
-	tmpl.ExecuteTemplate(w, "base", nil)
+	tmpl.ExecuteTemplate(w, "base", d)
 }
