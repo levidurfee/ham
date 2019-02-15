55acbb5e3364a4cb86db1ff42105970fd1d90a4e
Levi Durfee <levi.durfee@gmail.com>
2019-02-15 08:22:32 -0500

    Update RenderTemplate

--- handlers/templates.go
+++ handlers/templates.go
@@ -5,11 +5,11 @@ import (
 	"net/http"
 	"strconv"
 
-	"github.com/levidurfee/ham/hamlog"
+	"github.com/levidurfee/ham/models"
 )
 
 // RenderTemplate will write the template data to the browser
-func RenderTemplate(w http.ResponseWriter, d hamlog.GOhamData) {
+func RenderTemplate(w http.ResponseWriter, d models.PageData) {
 	w.Header().Set("Ham-Request-ID", strconv.FormatInt(d.RequestID, 10))
 	tmpl := template.Must(template.ParseFiles("templates/base.html", "templates/"+d.Template))
 	tmpl.ExecuteTemplate(w, "base", d)
