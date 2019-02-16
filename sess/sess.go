fb9fb9e12abb02b29db04eb1612d24e5d701b991
Levi Durfee <levi.durfee@gmail.com>
2019-02-16 14:07:16 -0500

    Extract get/save to own files

--- sess/sess.go
+++ sess/sess.go
@@ -9,31 +9,6 @@ import (
 
 const defaultSession = "hamsession"
 
-// Get a session variable
-func Get(ctx context.Context, w http.ResponseWriter, r *http.Request, key string) (interface{}, error) {
-	session, err := getSession(ctx, w, r)
-	if err != nil {
-		httpSessionError(w, err)
-		return nil, err
-	}
-
-	return session.Values[key], nil
-}
-
-// Save a session variable
-func Save(ctx context.Context, w http.ResponseWriter, r *http.Request, key string, value interface{}) error {
-	session, err := getSession(ctx, w, r)
-	if err != nil {
-		httpSessionError(w, err)
-		return err
-	}
-
-	session.Values[key] = value
-	session.Save(r, w)
-
-	return nil
-}
-
 func getSession(ctx context.Context, w http.ResponseWriter, r *http.Request) (*sessions.Session, error) {
 	sk, err := getSessionKey(ctx)
 	if err != nil {
