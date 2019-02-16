243a3fc5b1aa17f999f1f6f117e20a5298b08adc
Levi Durfee <levi.durfee@gmail.com>
2019-02-16 14:33:03 -0500

    Add params to BuildApp

--- repos/app.go
+++ repos/app.go
@@ -1,9 +1,13 @@
 package repos
 
-import "github.com/levidurfee/ham/models"
+import (
+	"net/http"
+
+	"github.com/levidurfee/ham/models"
+)
 
 // BuildApp constructs a struct with app data.
-func BuildApp() *models.App {
+func BuildApp(w http.ResponseWriter, r *http.Request) *models.App {
 	// First we'll create an App struct. We go ahead and set the LoggedIn field
 	// false as a default, we'll check cookies to see if they're logged in, and
 	// can update this field if they are logged in.
