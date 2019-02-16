1538b8a37fe7680bff98b13796093c07d4dd6b1a
Levi Durfee <levi.durfee@gmail.com>
2019-02-16 18:11:19 -0500

    Change return

--- repos/app.go
+++ repos/app.go
@@ -10,11 +10,11 @@ import (
 )
 
 // BuildApp constructs a struct with app data.
-func BuildApp(w http.ResponseWriter, r *http.Request) *models.App {
+func BuildApp(w http.ResponseWriter, r *http.Request) models.App {
 	// First we'll create an App struct. We go ahead and set the LoggedIn field
 	// false as a default, we'll check cookies to see if they're logged in, and
 	// can update this field if they are logged in.
-	app := &models.App{
+	app := models.App{
 		LoggedIn: false,
 	}
 
