0ac818d54a39ae39627faf5c891390f97fc194ea
Levi Durfee <levi.durfee@gmail.com>
2019-02-17 11:47:40 -0500

    Start over with main

--- _/main.go
+++ _/main.go
@@ -3,35 +3,17 @@ package main
 import (
 	"net/http"
 
-	"github.com/levidurfee/ham/handlers"
-
 	"github.com/gorilla/mux"
 	"google.golang.org/appengine"
-
-	firebase "firebase.google.com/go"
 )
-
-var (
-	firebaseConfig = &firebase.Config{
-		DatabaseURL:   "https://hamradio-96e65.firebaseio.com",
-		ProjectID:     "hamradio",
-		StorageBucket: "hamradio.appspot.com",
-	}
-)
-
-// TODO
-// * Create middleware that checks if user is logged in
-// * Set an encrypted cookie that says if they're logged in
-// * Cache their Firebase token in memcache
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", handlers.HomeHandler)
-	// r.HandleFunc("/login/", handlers.LoginHandler)
-	// r.HandleFunc("/tos/", handlers.TOSHandler)
-	// r.HandleFunc("/record-entry/", handlers.RecordEntryHandler)
-	// r.HandleFunc("/dashboard/", handlers.DashboardHandler)
-	http.Handle("/", r)
+	r.HandleFunc("/", homeHandler)
 
 	appengine.Main()
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hi"))
 }
